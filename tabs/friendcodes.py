507e1e845a114b791c3724e80468ea0d30e1f092
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2019-11-10 17:12:36 +0100

    add friend codes tab template

--- tabs/friendcodes.py
+++ tabs/friendcodes.py
@@ -13,3 +13,23 @@ class FriendcodesTab(Qw.QWidget):
         self.config = params.get('config')
         self.width = params.get('width')
         self.height = params.get('height')
+
+        self.create_tree()
+
+        self.layout = Qw.QVBoxLayout()
+        self.layout.addWidget(self.tree)
+
+        self.setLayout(self.layout)
+
+    def create_tree(self):
+        self.tree = Qw.QTreeWidget()
+        self.tree.setColumnCount(3)
+        self.tree.setHeaderLabels(['Console', 'Game', 'Friend Code'])
+
+        self.wii_entry = Qw.QTreeWidgetItem(['Wii', '-', '-'])
+        self.wiiware_entry = Qw.QTreeWidgetItem(['WiiWare', '-', '-'])
+        self.ds_entry = Qw.QTreeWidgetItem(['DS', '-', '-'])
+        self.dsiware_entry = Qw.QTreeWidgetItem(['DSiWare', '-', '-'])
+
+        self.tree.addTopLevelItems([self.wii_entry, self.wiiware_entry,
+                                    self.ds_entry, self.dsiware_entry])
