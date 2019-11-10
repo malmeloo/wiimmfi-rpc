d2c65845c2e59c34bb849ada3633c1328df06d4e
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2019-11-10 16:08:00 +0100

    add settings tab template

--- tabs/settings.py
+++ tabs/settings.py
@@ -13,3 +13,44 @@ class SettingsTab(Qw.QWidget):
         self.config = params.get('config')
         self.width = params.get('width')
         self.height = params.get('height')
+
+        self.create_update_groupbox()
+        self.create_danger_groupbox()
+
+        self.layout = Qw.QVBoxLayout()
+        self.layout.addWidget(self.update_group)
+        self.layout.addWidget(self.danger_group)
+
+        self.setLayout(self.layout)
+
+    def create_update_groupbox(self):
+        self.update_group = Qw.QGroupBox('Update Settings')
+
+        auto_download_label = Qw.QLabel('Enable auto download:')
+        self.auto_download = Qw.QCheckBox()
+        auto_install_label = Qw.QLabel('Enable auto install:')
+        self.auto_install = Qw.QCheckBox()
+        release_type_label = Qw.QLabel('Update to:')
+        self.release_type = Qw.QComboBox()
+        self.release_type.addItems(['Latest release', 'Beta', 'Alpha'])
+
+        layout = Qw.QFormLayout()
+        layout.addRow(auto_download_label, self.auto_download)
+        layout.addRow(auto_install_label, self.auto_install)
+        layout.addRow(release_type_label, self.release_type)
+        self.update_group.setLayout(layout)
+
+    def create_danger_groupbox(self):
+        self.danger_group = Qw.QGroupBox('Danger zone')
+        self.danger_group.setStyleSheet('QGroupBox {color: red}')
+
+        debug_label = Qw.QLabel('Enable debug mode:')
+        self.debug = Qw.QCheckBox()
+        self.clear_config = Qw.QPushButton('Clear config files')
+        self.clear_codes = Qw.QPushButton('Clear friend codes')
+
+        layout = Qw.QFormLayout()
+        layout.addRow(debug_label, self.debug)
+        layout.addRow(self.clear_config)
+        layout.addRow(self.clear_codes)
+        self.danger_group.setLayout(layout)
