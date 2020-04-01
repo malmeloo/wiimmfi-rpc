d63878bd79adb9130aeab289c4cd4291fed464a5
lmnrg <51184486+lmnrg@users.noreply.github.com>
2020-04-01 11:05:32 +0300

    run.bat file for windows users

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+title wiimmfi-rpc launcher
+echo                                 wiimmfi-rpc on windows
+echo                              ! DO NOT CLOSE THIS WINDOW !
+echo                                 ignore everything else
+echo ======================================================================================
+python rpcgui.py
+echo ======================================================================================
+echo             You have closed wiimmfi-rpc. You may close this window now.
+echo ======================================================================================
+pause
